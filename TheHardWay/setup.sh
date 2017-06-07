4f36c641b81443793b71912b3a18f593dd0b2465
xtophs <christoph@bracketangles.net>
2017-06-07 18:44:25 -0500

    use kubefed for setup to support beyond 1.6.x

---
+++ TheHardWay/setup.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+#
+# setup.sh
+#
+
+set -e
+
+if [[ $# != 2 ]]; then
+    echo Usage: setup.sh [cluster name] [zoneName]
+    exit 1
+fi  
+
+cluster=${1}
+
+cd kubernetes-cluster-federation
+
+mkdir -p clusters
+mkdir -p kubeconfigs
+mkdir -p kubeconfigs/${cluster}
+
+# protecting against 2nd run
+# we want to grab the pristine kubeconfig without any of the federation entries
+if [ ! -f kubeconfigs/${cluster}/kubeconfig ]
+then
+  cp ~/.kube/config kubeconfigs/${cluster}/kubeconfig
+fi 
+
+kubectl config use-context ${cluster}
+
+nodes=$(kubectl get nodes -o jsonpath='{.items[?(@.metadata.labels.role=="agent")].metadata.name}')
+echo found $nodes
+
+adminuser=$cluster-admin
+
+echo configuring context
+kubectl config set-context host-cluster \
+  --cluster=$cluster \
+  --user=${adminuser} \
+  --namespace=federation
+
+echo switching context
+kubectl config use-context host-cluster
+
+echo creating federation namespace
+kubectl create -f ns/federation.yaml
+
+echo created federation API server service
+kubectl create -f services/federation-apiserver.yaml
+
+echo creating federation token secret
+FEDERATION_TOKEN=$(head -c 16 /dev/urandom | od -An -t x | tr -d ' ')
+cat > known-tokens.csv <<EOF
+${FEDERATION_TOKEN},admin,admin
+EOF
+kubectl create secret generic federation-apiserver-secrets \
+  --from-file=known-tokens.csv
+
+kubectl create secret generic federation-controller-manager-dnsprovider-config \
+  --from-file=./dns.conf
+
+echo creating persistent volume
+kubectl create -f pvc/federation-apiserver-etcd.yaml
+
+echo waiting for apiserver service to come up
+count=0
+while [ $count -lt 20 ]
+do
+    echo checking for service $count
+    ip=$(kubectl get svc federation-apiserver -o jsonpath='{ .status.loadBalancer.ingress[0].ip }')
+    if [ -z $ip ]; then
+        sleep 60
+        count=$((count + 1))
+    else
+        break
+    fi
+done
+
+if [ -z $ip ];
+then 
+    echo api server service did not come up. Exiting
+    exit 1
+fi
+
+
+echo setting up configmap with ${ip}
+kubectl create configmap federated-apiserver \
+  --from-literal=advertise-address=${ip}
+
+kubectl get configmap federated-apiserver \
+  -o jsonpath='{.data.advertise-address}'
+
+echo spinning up federation server
+kubectl create -f deployments/federation-apiserver.yaml
+
+kubectl config set-cluster federation-cluster \
+  --server=https://${ip} --insecure-skip-tls-verify=true
+
+FEDERATION_TOKEN=$(cut -d"," -f1 known-tokens.csv)
+
+kubectl config set-credentials federation-cluster \
+  --token=${FEDERATION_TOKEN}
+
+kubectl config set-context federation-cluster \
+  --cluster=federation-cluster \
+  --user=federation-cluster
+
+kubectl config use-context federation-cluster
+
+mkdir -p kubeconfigs/federation-apiserver
+
+kubectl config view --flatten --minify > kubeconfigs/federation-apiserver/kubeconfig
+
+kubectl config use-context host-cluster
+
+kubectl create secret generic federation-apiserver-kubeconfig \
+  --from-file=kubeconfigs/federation-apiserver/kubeconfig
+
+kubectl create secret generic federation-controller-mamanger-dnsprovider-config \
+  --from-file=known-tokens.csv
+
+# should be a parameter
+export DNS_ZONE_NAME=${2}
+
+#has to be empty for Azure DNS
+export DNS_ZONE_ID=
+
+kubectl create configmap federation-controller-manager \
+  --from-literal=zone-id=${DNS_ZONE_ID} \
+  --from-literal=zone-name=${DNS_ZONE_NAME}
+
+count=0
+set +e 
+
+while [ $count -lt 20 ]
+do
+    echo waiting for apiserver to get ready $count
+    resp=$(curl -k https://${ip}:443)
+
+    if [ "$resp" == "Unauthorized" ];
+    then
+        break
+    fi 
+    count=$((count + 1))
+done
+
+set -e
+    
+kubectl create -f deployments/federation-controller-manager.yaml
+
+set +e 
+count=0
+while [ $count -lt 20 ]
+do
+    echo waiting for federation controller manager $count
+    status=$(kubectl get pods -o jsonpath='{ .items[?(@.metadata.labels.module=="federation-controller-manager")].status.phase }')
+    if [ "$status" == "Running" ];
+    then
+        break
+    fi 
+    sleep 60
+    count=$((count + 1))
+done
+
+echo Federation Controller Manager Running
+set -e
+kubectl config use-context federation-cluster
+mkdir -p configmaps
+cat > configmaps/kube-dns.yaml <<EOF
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: kube-dns
+  namespace: kube-system
+data:
+  federations: federation=${DNS_ZONE_NAME}
+EOF
+
+count=0
+
+set +e
+while [ $count -lt 20 ]
+do 
+  result=$(kubectl create -f configmaps/kube-dns.yaml | grep created)
+  if [[ -n $result ]]; then
+    break
+  fi
+  count=$(( count + 1 ))
+done
+
+echo DNS configmap created
+cat configmaps/kube-dns.yaml
+
+echo SUCCESS
---
+++ setup-fedhost.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#
+# setup.sh
+#
+
+if [[ $# != 2 ]]; then
+    echo Usage: setup-fedhost.sh [cluster name] [zoneName]
+    exit 1
+fi  
+
+binding=$(kubectl get clusterrolebinding permissive-binding)
+if [[ -z $binding ]]; then
+  echo creating permissive binding. NOT recommended for production clusters
+  kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=client --group=system:serviceaccounts
+fi
+
+result=$(grep 'authorization-mode=RBAC' /etc/kubernetes/manifests/kube-apiserver.yaml)
+if [[ -z $result ]]; then
+  echo Kube API server not configured for RBAC
+  echo add --authorization-mode=RBAC to the hyperkube startup args in /etc/kubernetes/manifests/kube-apiserver.yaml
+  echo end then reboot
+  exit 1
+fi
+
+cluster=${1}
+fedName=myfederation
+
+set -e
+
+curl -LO https://dl.k8s.io/v1.7.0-alpha.4/kubernetes-client-linux-amd64.tar.gz
+tar xzf kubernetes-client-linux-amd64.tar.gz
+
+sudo cp kubernetes/client/bin/kubefed /usr/local/bin
+sudo chmod +x /usr/local/bin/kubefed
+sudo cp kubernetes/client/bin/kubectl /usr/local/bin
+sudo chmod +x /usr/local/bin/kubectl
+
+wd=$(pwd)
+kubefed init ${fedName} --dns-provider="azure-azuredns" --dns-zone-name=${2} --dns-provider-config=${wd}/dns.conf --image=xtoph/hyperkube-amd64:azuredns.20
+
+# double check
+ns=$(kubectl get namespace --context=myfederation)
+if [[ -n $binding ]]; then
+  kubectl create namespace default --context=${fedName}
+fi
+
+kubectl config use-context ${fedName}
+
+
+kubefed join ${cluster} --host-cluster-context=${cluster} --cluster-context=${cluster}
+
+
+git clone https://github.com/OguzPastirmaci/hostname
+
+kubectl get clusters
+
+echo export KUBECONFIG=~/.kube/config >> .bashrc
+
+echo SUCCESS
